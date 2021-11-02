d6fec5ee6868062f5e5e7833db0a33df833059d7
juanjo-nan <57089222+juanjo-nan@users.noreply.github.com>
2021-11-02 14:22:24 +0100

    Dont add values of canceled invoices. (#3)
    Task #047897

--- taxes_by_invoice/taxes_by_invoice.py
+++ taxes_by_invoice/taxes_by_invoice.py
@@ -299,6 +299,11 @@ class TaxesByInvoiceReport(HTMLReport):
                 records.setdefault(tax.invoice.move.period, []).append(
                     DualRecord(tax))
 
+                # If the invoice is cancelled, do not add its values to the
+                # totals
+                if data['include_cancel'] and tax.invoice.state == 'cancel':
+                    continue
+
                 # With this we have the total for each tax (total base, total
                 # amount and total)
                 tax_totals.setdefault(tax.invoice.move.period, {
