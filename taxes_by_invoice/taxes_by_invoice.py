19a9f94f35eb708a98da01c4fca3792ec1968a39
Raimon Esteve <raimonesteve@gmail.com>
2025-06-20 07:05:21 +0200

    Replace output format xls to xlsx
    #178030

--- taxes_by_invoice/taxes_by_invoice.py
+++ taxes_by_invoice/taxes_by_invoice.py
@@ -59,7 +59,7 @@ class PrintTaxesByInvoiceAndPeriodStart(ModelView):
     output_format = fields.Selection([
             ('pdf', 'PDF'),
             ('html', 'HTML'),
-            ('xls', 'Excel'),
+            ('xlsx', 'Excel'),
             ], 'Output Format', required=True)
     company = fields.Many2One('company.company', 'Company', required=True)
     start_date = fields.Date('Initial posting date',
