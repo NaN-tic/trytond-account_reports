f17a9c41bc5b5360cc371427c0ba9901a0748b30
Raimon Esteve <raimonesteve@gmail.com>
2026-03-17 14:42:14 +0100

    Abreviated and Journal reports (#24)
    #185190

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@
 # copyright notices and license terms.
 from trytond.pool import Pool
 from . import common
+from . import abreviated_journal
 from . import general_ledger
 from . import taxes_by_invoice
 from . import trial_balance
@@ -15,18 +16,22 @@ def register():
         common.Account,
         common.Party,
         common.FiscalYear,
+        abreviated_journal.PrintAbreviatedJournalStart,
         general_ledger.PrintGeneralLedgerStart,
         taxes_by_invoice.PrintTaxesByInvoiceAndPeriodStart,
         trial_balance.PrintTrialBalanceStart,
         journal.PrintJournalStart,
         module=module, type_='model')
     Pool.register(
+        abreviated_journal.PrintAbreviatedJournal,
         general_ledger.PrintGeneralLedger,
         taxes_by_invoice.PrintTaxesByInvoiceAndPeriod,
         trial_balance.PrintTrialBalance,
         journal.PrintJournal,
         module=module, type_='wizard')
     Pool.register(
+        abreviated_journal.AbreviatedJournalReport,
+        abreviated_journal.AbreviatedJournalXlsxReport,
         journal.JournalReport,
         journal.JournalXlsxReport,
         general_ledger.GeneralLedgerReport,
