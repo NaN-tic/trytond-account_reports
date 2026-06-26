3fd07b339c1ec5dc302fa000befd3ae8632021d1
Àngel Àlvarez <angel@nan-tic.com>
2026-06-26 14:55:08 +0200

    add report to show payment_days of invoices

--- __init__.py
+++ __init__.py
@@ -4,10 +4,15 @@ from trytond.pool import Pool
 from . import common
 from . import abreviated_journal
 from . import general_ledger
-from . import taxes_by_invoice
-from . import trial_balance
 from . import journal
 from . import open_move_lines
+from . import taxes_by_invoice
+from . import trial_balance
+from .invoice_payment_dates.invoice_payment_dates import (
+    InvoicePaymentDatesReport,
+    PrintInvoicePaymentDates,
+    PrintInvoicePaymentDatesStart,
+    )
 
 
 def register():
@@ -19,30 +24,33 @@ def register():
         common.FiscalYear,
         abreviated_journal.PrintAbreviatedJournalStart,
         general_ledger.PrintGeneralLedgerStart,
-        taxes_by_invoice.PrintTaxesByInvoiceAndPeriodStart,
-        trial_balance.PrintTrialBalanceStart,
         journal.PrintJournalStart,
         open_move_lines.PrintOpenMoveLinesStart,
+        taxes_by_invoice.PrintTaxesByInvoiceAndPeriodStart,
+        trial_balance.PrintTrialBalanceStart,
+        PrintInvoicePaymentDatesStart,
         module=module, type_='model')
     Pool.register(
         abreviated_journal.PrintAbreviatedJournal,
         general_ledger.PrintGeneralLedger,
-        taxes_by_invoice.PrintTaxesByInvoiceAndPeriod,
-        trial_balance.PrintTrialBalance,
         journal.PrintJournal,
         open_move_lines.PrintOpenMoveLines,
+        taxes_by_invoice.PrintTaxesByInvoiceAndPeriod,
+        trial_balance.PrintTrialBalance,
+        PrintInvoicePaymentDates,
         module=module, type_='wizard')
     Pool.register(
         abreviated_journal.AbreviatedJournalReport,
         abreviated_journal.AbreviatedJournalXlsxReport,
-        journal.JournalReport,
-        journal.JournalXlsxReport,
         general_ledger.GeneralLedgerReport,
         general_ledger.GeneralLedgerXlsxReport,
+        journal.JournalReport,
+        journal.JournalXlsxReport,
+        open_move_lines.OpenMoveLinesReport,
+        open_move_lines.OpenMoveLinesXlsxReport,
         taxes_by_invoice.TaxesByInvoiceReport,
         taxes_by_invoice.TaxesByInvoiceXlsxReport,
         trial_balance.TrialBalanceReport,
         trial_balance.TrialBalanceXlsxReport,
-        open_move_lines.OpenMoveLinesReport,
-        open_move_lines.OpenMoveLinesXlsxReport,
+        InvoicePaymentDatesReport,
         module=module, type_='report')
